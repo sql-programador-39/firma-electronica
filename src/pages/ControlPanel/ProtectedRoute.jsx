b65196ca77c68dca360f4dd88db33d536540d572
sql-programador-39 <sql.programador39@opa.com.co>
2024-02-15 10:36:14 -0500

    more errors

--- src/pages/ControlPanel/ProtectedRoute.jsx
+++ src/pages/ControlPanel/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom"
-import { useAuth } from "../../auth/authProvider"
+import { useAuth } from "../../auth/AuthProvider"
 
 const ProtectedRoute = () => {
 
